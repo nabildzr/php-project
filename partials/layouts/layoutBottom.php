46fd1e08c5628fe9c23d4d4dee66c888ffa97b2c
Rixcog <riqzanramdhan@gmail.com>
2024-11-12 07:52:35 +0700

    menghapus semua repository

--- partials/layouts/layoutBottom.php
+++
@@ -1,9 +0,0 @@
-</div>
-
-<?php include_once($_SERVER['DOCUMENT_ROOT'] . '/admin-restaurant/partials/footer.php') ?>
-</main>
-
-<?php include_once($_SERVER['DOCUMENT_ROOT'] . '/admin-restaurant/partials/scripts.php') ?>
-</body>
-
-</html>
